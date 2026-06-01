bc0fdbbf16f2467fc9f067c7d49e47ceb22cc624
Emmy Abiodun <emmyabiodun0@gmail.com>
2026-06-01 14:39:42 +0100

    update

--- utils.py
+++ utils.py
@@ -1,11 +1,49 @@
+import os
 import secrets
 import string
 
+from flask import json
+import requests
+from dotenv import load_dotenv
 
+BREVO_URL= "https://api.brevo.com/v3/smtp/email"
 
 def generate_random_otp(lenght: int):
     """Generate random number"""
     random_digits = [secrets.choice(string.digits) for _ in range(lenght)]
     return "".join(random_digits)
+
+
+def send_registration_mail(
+    to:str,
+    username:str,
+    otp:str,
+    html_content:str
+    ):
+    payload = {  
+            "sender":{  
+                "name":"Flask App",
+                "email":"gemmy1866@gmail.com"
+            },
+            "to":[  
+            {  
+                "email":to,
+                "name":username
+            }
+            ],
+                "subject":f"Verifiy Account: Your OTP is {otp}",
+                "htmlContent": html_content
+            }
+    response = requests.post(
+            url=BREVO_URL,
+            headers={
+                'accept': "application/json",
+                'content-type':"application/json",
+                'api-key':os.getenv("BREVO_API_KEY")
+            },
+            data=json.dumps(payload)
+        )
+    print(response.json())
+    return response
 
 
